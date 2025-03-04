e2961d5a437efdc8120d2d86d38d474d5a90d20b
Andrej <sir_aak@hotmail.com>
2025-03-04 12:34:56 -0500

    add scenarios and behave tests

--- features/steps/load_steps.py
+++ features/steps/load_steps.py
@@ -30,7 +30,7 @@ HTTP_200_OK = 200
 HTTP_201_CREATED = 201
 HTTP_204_NO_CONTENT = 204
 
-@given('the following products')
+@given("the following products")
 def step_impl(context):
     """ Delete all Products and load new ones """
     #
@@ -47,6 +47,12 @@ def step_impl(context):
     # load the database with new products
     #
     for row in context.table:
-        #
-        # ADD YOUR CODE HERE TO CREATE PRODUCTS VIA THE REST API
-        #
+        payload = {
+            "name": row["name"],
+            "description": row["description"],
+            "price": row["price"],
+            "available": row["available"] in ["True", "true", "1"],
+            "category": row["category"]
+        }
+        context.resp = requests.post(rest_endpoint, json=payload)
+        assert context.resp.status_code == HTTP_201_CREATED
